d603eaae9eb4c52bdbf40c4a69dd36267d9e303c
Владислав <65997869+lfc2020yes@users.noreply.github.com>
2022-11-10 16:17:14 +0400

    обновление входа

--- template/form_js.php
+++ template/form_js.php
@@ -5,10 +5,10 @@
 $local='C:/OpenServer/domains/'.$local_host.'';
 if($_SERVER['DOCUMENT_ROOT']!=$local)
 {
-echo'<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1668082245795"></script>'; 
+echo'<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1668082600998"></script>'; 
 } else
 {
-echo'<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1668082245795"></script>';	
+echo'<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1668082600998"></script>';	
 }
 
 	
