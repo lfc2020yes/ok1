0ca17afe0d71f718dfcd8c17f8e7c4d3bb774dc1
Владислав <65997869+lfc2020yes@users.noreply.github.com>
2023-01-16 13:38:12 +0400

    бонусы исправление

--- CRON/bonus_new_trips.php
+++ CRON/bonus_new_trips.php
@@ -217,7 +217,40 @@ if($_GET["m"]=='now')
     $date_end=date_step_sql('Y-m','+1m').'-01 00:00:00';
     $month_s=date("Y-m-").'01';
 
+    $result_8 = mysql_time_query($link,'SELECT A.id FROM r_user AS A');
+    $num_8 = $result_8->num_rows;
+    if($result_8)
+    {
+        while($row_8 = mysqli_fetch_assoc($result_8)){
+            $commission=0;
 
+            $result_status21=mysql_time_query($link,'SELECT sum(a.commission) as comm FROM trips AS a WHERE  a.status=1 and a.commission_fix=0 and a.visible=1 and a.id_user="'.$row_8["id"].'" and a.date_buy_all>="'.$date_start.'" and a.date_buy_all<"'.$date_end.'"');
+
+            if($result_status21->num_rows!=0)
+            {
+                $row_status21 = mysqli_fetch_assoc($result_status21);
+                //есть ли запись с такой коммиссией по этому пользователю за данный месяц
+                $result_status22=mysql_time_query($link,'SELECT a.id from users_commission_trips as a where a.id_users="'.$row_8["id"].'" and a.date="'.$month_s.'"');
+
+                if($row_status21["comm"]=='')
+                {
+                    $row_status21["comm"]=0;
+                }
+
+                if($result_status22->num_rows!=0)
+                {
+                    mysql_time_query($link,'update users_commission_trips set sum="'.$row_status21["comm"].'" where id_users="'.$row_8["id"].'" and date="'.$month_s.'"');
+
+                } else
+                {
+                    mysql_time_query($link,'INSERT INTO users_commission_trips (id_users,date,sum) VALUES ("'.$row_8["id"].'","'.$month_s.'","'.$row_status21["comm"].'")');
+                }
+            }
+
+        }
+    }
+
+//ПЕРЕСЧЕТ ФИКСИРОВАННОЙ КОМИССИИ У ПОЛЬЗОВАТЕЛЕЙ ЗА ПРОШЛЫЙ МЕСЯЦ
     $result_8 = mysql_time_query($link,'SELECT A.id FROM r_user AS A');
     $num_8 = $result_8->num_rows;
     if($result_8)
@@ -243,6 +276,7 @@ if($_GET["m"]=='now')
                 {
                     $row_status21["comm1"]=0;
                 }
+
                 if($result_status22->num_rows!=0)
                 {
                     mysql_time_query($link,'update users_commission_trips set sum_fix="'.$row_status21["comm"].'",sum_com="'.$row_status21["comm1"].'" where id_users="'.$row_8["id"].'" and date="'.$month_s.'"');
@@ -257,6 +291,7 @@ if($_GET["m"]=='now')
 
         }
     }
+
 	echo('Бонусы за текущий месяц обновлены!');
 }
 
