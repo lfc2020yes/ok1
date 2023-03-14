4d5aeaeb9a950116a17d0f43737f096b5bbaddff
Владислав <65997869+lfc2020yes@users.noreply.github.com>
2023-03-14 12:01:02 +0400

    обращения перенаправить

--- template/form_js.php
+++ template/form_js.php
@@ -5,10 +5,10 @@
 $local='C:/OpenServer/domains/'.$local_host.'';
 if($_SERVER['DOCUMENT_ROOT']!=$local)
 {
-echo'<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1678778957768"></script>'; 
+echo'<script language="JavaScript" type="text/javascript" src="/public/forms.map.min.js?cb=1678780293640"></script>'; 
 } else
 {
-echo'<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1678778957768"></script>';	
+echo'<script language="JavaScript" type="text/javascript" src="/public/forms.map.js?cb=1678780293640"></script>';	
 }
 
 	
